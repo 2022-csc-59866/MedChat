61e31bc01a2fe8f28660d394b491387f410bbe47
Shahjalal Sikder <shah.sikder55@gmail.com>
2023-04-10 11:46:15 -0400

    assets

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -3,7 +3,8 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-import HospitalIcon from '../assets/hospital.png'
+import HospitalIcon from '../assets/hospital.png';
+import LogoutIcon from '../assets/logout.png';
 
 const SideBar = () => (
     <div className='channel-list__sidebar'>
@@ -12,14 +13,19 @@ const SideBar = () => (
                 <img src='{HospitalIcon}' alt='Hospital' width='30' />
             </div>
         </div>
+        <div className='channel-list__sidebar__icon2'>
+            <div className='icon1__inner'>
+                <img src='{LogoutIcon}' alt='Logout' width='30' />
+            </div>
+        </div>
     </div>
 )
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            ChannelListContainer
-        </div>
+        <>
+            <SideBar />
+        </>
     )
 }
 
