d6c34e03a122ad561f0cea567b3ea977ab44c00a
Shahjalal Sikder <shah.sikder55@gmail.com>
2023-04-10 12:32:26 -0400

    Worked on channel search

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ChannelList, useChatContext } from 'stream-chat-react';
+import { ChannelList, ChannelPreviewMessenger, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
@@ -10,14 +10,20 @@ const SideBar = () => (
     <div className='channel-list__sidebar'>
         <div className='channel-list__sidebar__icon1'>
             <div className='icon1__inner'>
-                <img src='{HospitalIcon}' alt='Hospital' width='30' />
+                <img src={HospitalIcon} alt='Hospital' width='30' />
             </div>
         </div>
         <div className='channel-list__sidebar__icon2'>
             <div className='icon1__inner'>
-                <img src='{LogoutIcon}' alt='Logout' width='30' />
+                <img src={LogoutIcon} alt='Logout' width='30' />
             </div>
         </div>
+    </div>
+)
+
+const CompanyHeader = () => (
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>Medical Pager</p>
     </div>
 )
 
@@ -25,6 +31,10 @@ const ChannelListContainer = () => {
     return (
         <>
             <SideBar />
+            <div className='channel-list__list__wrapper'>
+                <CompanyHeader />
+                <ChannelSearch />
+            </div>
         </>
     )
 }
